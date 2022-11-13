b2f3cd599dc0aff05d6f4421bdf0ec27611b4794
Sherbek <amirovsherbek1205@gmail.com>
2022-11-13 15:08:46 +0300

    first commit

---
+++ src/kurslar/kurs.jsx
@@ -0,0 +1,77 @@
+import './kurs.css';
+function Kurs(){
+   return (
+         <div className="kurs">
+            <div className="kurs_them">
+                <h1>Bizning kurslar</h1>
+                <div>Bizning kurslar orqali IT kasblarini
+                     tez va oson o’rganing 
+                </div>
+            </div>
+           <div className="kurslar">
+             <div className="k-card">
+                <div className="card mx-1 my-2 ">                  
+                     <img src="https://brunoyam.com/storage/post/37/9b9b7f01c880573efaf20d234b1eea57.png" alt="motion dizayn" />
+                   <div className="card-header">
+                     <h4  className='card-title'>
+                        Motion dizayn
+                     </h4>
+                  </div>
+                  <div className="card-body">
+                     <div>Motion dizayn kursimiz  davomida siz
+                         dasturlash mantigʻi, uning algoritmi
+                      </div>
+                  </div>
+                </div>
+             </div>
+             <div className="k-card">
+             <div className="card mx-1 my-2 ">                  
+                     <img src="https://brunoyam.com/storage/post/37/9b9b7f01c880573efaf20d234b1eea57.png" alt="motion dizayn" />
+                   <div className="card-header">
+                     <h4  className='card-title'>
+                        Motion dizayn
+                     </h4>
+                  </div>
+                  <div className="card-body">
+                     <div>Motion dizayn kursimiz  davomida siz
+                         dasturlash mantigʻi, uning algoritmi
+                      </div>
+                  </div>
+                </div>
+             </div>
+             <div className="k-card">
+                <div className="card mx-1 my-2 ">                  
+                     <img src="https://brunoyam.com/storage/post/37/9b9b7f01c880573efaf20d234b1eea57.png" alt="motion dizayn" />
+                   <div className="card-header">
+                     <h4  className='card-title'>
+                        Motion dizayn
+                     </h4>
+                  </div>
+                  <div className="card-body">
+                     <div>Motion dizayn kursimiz  davomida siz
+                         dasturlash mantigʻi, uning algoritmi
+                      </div>
+                  </div>
+                </div>
+             </div>
+             <div className="k-card">
+                <div className="card mx-1 my-2 ">                  
+                     <img src="https://brunoyam.com/storage/post/37/9b9b7f01c880573efaf20d234b1eea57.png" alt="motion dizayn" />
+                   <div className="card-header">
+                     <h4  className='card-title'>
+                        Motion dizayn
+                     </h4>
+                  </div>
+                  <div className="card-body">
+                     <div>Motion dizayn kursimiz  davomida siz
+                         dasturlash mantigʻi, uning algoritmi
+                      </div>
+                  </div>
+                </div>
+             </div>
+           </div>
+           
+         </div>
+    )
+}
+export default Kurs
