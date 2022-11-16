5e9323a86694b4d2fd95f23b1f4ecd642dcefe3a
Sherbek <amirovsherbek1205@gmail.com>
2022-11-16 13:31:40 +0300

    editing

--- src/footer/footer.jsx
+++ src/footer/footer.jsx
@@ -1,5 +1,4 @@
 import './footer.css'
-
 function Footer(){
     return(
         <div className="footer">
@@ -10,9 +9,9 @@ function Footer(){
               </div>
            </div>
            <div className="form_users">
-              <input type="text" className='form' placeholder='Ism sharifingiz' />
-              <input type="text" className='form' placeholder='Tel raqam' />
-              <select className='form' >
+               <input type="text" className='form' placeholder='Ism sharifingiz' />
+               <input type="text" className='form' placeholder='Tel raqam' />
+               <select className='form' >
                  <option value="fronted developer">fronted developer</option>
                  <option value="Backend developer">Backend developer</option>
                  <option value="Full stack developer">Full stack developer</option>
@@ -20,10 +19,10 @@ function Footer(){
                  <option value="Full stack developer">Full stack developer</option>
                  <option value="Backend developer">Backend developer</option>
                  <option value="SMM">SMM</option>
-              </select>
-              <button className='btnx btn_add' type={"button"}>Darsimizga yozilish !</button>
-           </div>
-     </div>
+               </select>
+               <button className='btnx btn_add' type={"button"}>Darsimizga yozilish !</button>
+            </div>
+        </div>
     )
 }
 export default Footer;
