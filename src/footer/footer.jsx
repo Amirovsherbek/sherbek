c0af307771296ea6468200d01fdcd29cd336fc24
Sherbek <amirovsherbek1205@gmail.com>
2022-11-16 13:21:05 +0300

    editing

--- src/footer/footer.jsx
+++ src/footer/footer.jsx
@@ -3,13 +3,13 @@ import './footer.css'
 function Footer(){
     return(
         <div className="footer">
-        <div className="footer_title">
-             <div>Bepul ochiq darsda qatnashing!</div>
-             <div>   Bepul darsimizda qatnashib ko’ring, sizga 
+            <div className="footer_title">
+              <div>Bepul ochiq darsda qatnashing!</div>
+              <div>   Bepul darsimizda qatnashib ko’ring, sizga 
                      yoqsa o’quvchimizga aylanishingiz mumkin!
-             </div>
-        </div>
-        <div className="form_users">
+              </div>
+           </div>
+           <div className="form_users">
               <input type="text" className='form' placeholder='Ism sharifingiz' />
               <input type="text" className='form' placeholder='Tel raqam' />
               <select className='form' >
@@ -21,8 +21,8 @@ function Footer(){
                  <option value="Backend developer">Backend developer</option>
                  <option value="SMM">SMM</option>
               </select>
-              <button className='btn btn_add' type={"button"}>Darsimizga yozilish !</button>
-        </div>
+              <button className='btnx btn_add' type={"button"}>Darsimizga yozilish !</button>
+           </div>
      </div>
     )
 }
