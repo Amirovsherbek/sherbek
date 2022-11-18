f108668053fb68f090fbada9ea9dbd7e2de8ea03
Sherbek <amirovsherbek1205@gmail.com>
2022-11-18 03:20:45 +0300

    editing

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -8,102 +8,114 @@ function About(){
                  <div>Bizning kurslar orqali IT kasblarini tez va oson o’rganing </div>
             </div>
             <div className="about_box">
-                     <div className="card w-100">
-                        <div className="card-header">
-                           <div className="about_card_avatar">
-                               <img src="https://e7.pngegg.com/pngimages/340/946/png-clipart-avatar-user-computer-icons-software-developer-avatar-child-face-thumbnail.png" alt="error" />
-                           </div>   
-                           <div className="card-title">
-                               <div>Gulsevar Usmonova</div>
+                    <div className="about_card">
+                      <div className="card w-100">
+                           <div className="card-header d-flex">
+                               <div className="about_card_avatar">
+                                   <img src="https://e7.pngegg.com/pngimages/340/946/png-clipart-avatar-user-computer-icons-software-developer-avatar-child-face-thumbnail.png" alt="error" />
+                               </div>   
+                               <div className="card-title w-70  ">
+                                  <div>Gulsevar Usmonova</div>
+                                </div>
                            </div>
-                       </div>
-                       <div className="card-body">
-                           <div className='card-text'>Bu kurslar menga juda yoqdi. Barchaga sotib olishni
-                                 maslahat beraman.Registon IT School
-                                juda sifatli o’quv markaz deb bilaman men.
-                           </div>
-                       </div>
-                    </div>     
-                    <div className="card w-100">
-                        <div className="card-header">
-                           <div className="about_card_avatar">
-                               <img src="https://e7.pngegg.com/pngimages/340/946/png-clipart-avatar-user-computer-icons-software-developer-avatar-child-face-thumbnail.png" alt="error" />
-                           </div>   
-                           <div className="card-title">
-                               <div>Gulsevar Usmonova</div>
-                           </div>
-                       </div>
-                       <div className="card-body">
-                           <div className='card-text'>Bu kurslar menga juda yoqdi. Barchaga sotib olishni
-                                 maslahat beraman.Registon IT School
-                                juda sifatli o’quv markaz deb bilaman men.
-                           </div>
-                       </div>
+                            <div className="card-body">
+                                 <div className='card-text'>Bu kurslar menga juda yoqdi. Barchaga sotib olishni
+                                    maslahat beraman.Registon IT School
+                                   juda sifatli o’quv markaz deb bilaman men.
+                                 </div>
+                            </div>
+                      </div> 
                     </div>
-                    <div className="card w-100">
-                        <div className="card-header">
-                           <div className="about_card_avatar">
-                               <img src="https://e7.pngegg.com/pngimages/340/946/png-clipart-avatar-user-computer-icons-software-developer-avatar-child-face-thumbnail.png" alt="error" />
-                           </div>   
-                           <div className="card-title">
-                               <div>Gulsevar Usmonova</div>
+                    <div className="about_card">
+                      <div className="card w-100">
+                           <div className="card-header d-flex">
+                               <div className="about_card_avatar">
+                                   <img src="https://e7.pngegg.com/pngimages/340/946/png-clipart-avatar-user-computer-icons-software-developer-avatar-child-face-thumbnail.png" alt="error" />
+                               </div>   
+                               <div className="card-title w-70  ">
+                                  <div>Gulsevar Usmonova</div>
+                                </div>
                            </div>
-                       </div>
-                       <div className="card-body">
-                           <div className='card-text'>Bu kurslar menga juda yoqdi. Barchaga sotib olishni
-                                 maslahat beraman.Registon IT School
-                                juda sifatli o’quv markaz deb bilaman men.
+                            <div className="card-body">
+                                 <div className='card-text'>Bu kurslar menga juda yoqdi. Barchaga sotib olishni
+                                    maslahat beraman.Registon IT School
+                                   juda sifatli o’quv markaz deb bilaman men.
+                                 </div>
+                            </div>
+                      </div> 
+                    </div>   
+                    <div className="about_card">
+                      <div className="card w-100">
+                           <div className="card-header d-flex">
+                               <div className="about_card_avatar">
+                                   <img src="https://e7.pngegg.com/pngimages/340/946/png-clipart-avatar-user-computer-icons-software-developer-avatar-child-face-thumbnail.png" alt="error" />
+                               </div>   
+                               <div className="card-title w-70  ">
+                                  <div>Gulsevar Usmonova</div>
+                                </div>
                            </div>
-                       </div>
+                            <div className="card-body">
+                                 <div className='card-text'>Bu kurslar menga juda yoqdi. Barchaga sotib olishni
+                                    maslahat beraman.Registon IT School
+                                   juda sifatli o’quv markaz deb bilaman men.
+                                 </div>
+                            </div>
+                      </div> 
                     </div>
-                    <div className="card w-100">
-                        <div className="card-header">
-                           <div className="about_card_avatar">
-                               <img src="https://e7.pngegg.com/pngimages/340/946/png-clipart-avatar-user-computer-icons-software-developer-avatar-child-face-thumbnail.png" alt="error" />
-                           </div>   
-                           <div className="card-title">
-                               <div>Gulsevar Usmonova</div>
+                    <div className="about_card">
+                      <div className="card w-100">
+                           <div className="card-header d-flex">
+                               <div className="about_card_avatar">
+                                   <img src="https://e7.pngegg.com/pngimages/340/946/png-clipart-avatar-user-computer-icons-software-developer-avatar-child-face-thumbnail.png" alt="error" />
+                               </div>   
+                               <div className="card-title w-70  ">
+                                  <div>Gulsevar Usmonova</div>
+                                </div>
                            </div>
-                       </div>
-                       <div className="card-body">
-                           <div className='card-text'>Bu kurslar menga juda yoqdi. Barchaga sotib olishni
-                                 maslahat beraman.Registon IT School
-                                juda sifatli o’quv markaz deb bilaman men.
-                           </div>
-                       </div>
+                            <div className="card-body">
+                                 <div className='card-text'>Bu kurslar menga juda yoqdi. Barchaga sotib olishni
+                                    maslahat beraman.Registon IT School
+                                   juda sifatli o’quv markaz deb bilaman men.
+                                 </div>
+                            </div>
+                      </div> 
                     </div>
-                    <div className="card w-100">
-                        <div className="card-header">
-                           <div className="about_card_avatar">
-                               <img src="https://e7.pngegg.com/pngimages/340/946/png-clipart-avatar-user-computer-icons-software-developer-avatar-child-face-thumbnail.png" alt="error" />
-                           </div>   
-                           <div className="card-title">
-                               <div>Gulsevar Usmonova</div>
+                    <div className="about_card">
+                      <div className="card w-100">
+                           <div className="card-header d-flex">
+                               <div className="about_card_avatar">
+                                   <img src="https://e7.pngegg.com/pngimages/340/946/png-clipart-avatar-user-computer-icons-software-developer-avatar-child-face-thumbnail.png" alt="error" />
+                               </div>   
+                               <div className="card-title w-70  ">
+                                  <div>Gulsevar Usmonova</div>
+                                </div>
                            </div>
-                       </div>
-                       <div className="card-body">
-                           <div className='card-text'>Bu kurslar menga juda yoqdi. Barchaga sotib olishni
-                                 maslahat beraman.Registon IT School
-                                juda sifatli o’quv markaz deb bilaman men.
+                            <div className="card-body">
+                                 <div className='card-text'>Bu kurslar menga juda yoqdi. Barchaga sotib olishni
+                                    maslahat beraman.Registon IT School
+                                   juda sifatli o’quv markaz deb bilaman men.
+                                 </div>
+                            </div>
+                      </div> 
+                    </div>   
+                    <div className="about_card">
+                      <div className="card w-100">
+                           <div className="card-header d-flex">
+                               <div className="about_card_avatar">
+                                   <img src="https://e7.pngegg.com/pngimages/340/946/png-clipart-avatar-user-computer-icons-software-developer-avatar-child-face-thumbnail.png" alt="error" />
+                               </div>   
+                               <div className="card-title w-70  ">
+                                  <div>Gulsevar Usmonova</div>
+                                </div>
                            </div>
-                       </div>
-                    </div>
-                    <div className="card w-100">
-                        <div className="card-header">
-                           <div className="about_card_avatar">
-                               <img src="https://e7.pngegg.com/pngimages/340/946/png-clipart-avatar-user-computer-icons-software-developer-avatar-child-face-thumbnail.png" alt="error" />
-                           </div>   
-                           <div className="card-title">
-                               <div>Gulsevar Usmonova</div>
-                           </div>
-                       </div>
-                       <div className="card-body">
-                           <div className='card-text'>Bu kurslar menga juda yoqdi. Barchaga sotib olishni
-                                 maslahat beraman.Registon IT School
-                                juda sifatli o’quv markaz deb bilaman men.
-                           </div>
-                       </div>
-                    </div>                       
+                            <div className="card-body">
+                                 <div className='card-text'>Bu kurslar menga juda yoqdi. Barchaga sotib olishni
+                                    maslahat beraman.Registon IT School
+                                   juda sifatli o’quv markaz deb bilaman men.
+                                 </div>
+                            </div>
+                      </div> 
+                    </div>      
             </div>
         </div>
     )
--- src/navbar/Navbar.jsx
+++ src/navbar/Navbar.jsx
@@ -3,27 +3,32 @@ import './navbar.css'
 import Logotip from '../image/logotip.png'
 import { useState } from 'react';
 import {GiHamburgerMenu} from 'react-icons/gi'
+import {Modal,ModalHeader,ModalBody,ModalFooter,Button} from 'reactstrap'
 function Navbar() {
   const [hamburger,setHamburger]=useState(false)
+  const isopen = () => setHamburger(!hamburger);
   return (
       <div className='navbar' >
-        <div className="navbar_logotip">
-          <img src={Logotip} alt="error" />
-        </div>
-      <div className="navbar_link ">
-      <NavLink to={'/kurslarimiz'} className={'nav_link'}> Kurslarimiz</NavLink>
-      <NavLink to={'/haqimizda'} className={'nav_link'}> Biz haqimizda</NavLink>
-      <NavLink to={'kirish'} className={'nav_link btnx btn_nav_link'} > Kirish</NavLink>
-      </div>
+          <div className="navbar_logotip">
+                 <img src={Logotip} alt="error" />
+          </div>
+          <div className="navbar_link ">
+               <NavLink to={'/kurslarimiz'} className={'nav_link'}> Kurslarimiz</NavLink>
+               <NavLink to={'/haqimizda'} className={'nav_link'}> Biz haqimizda</NavLink>
+               <NavLink to={'kirish'} className={'nav_link btnx btn_nav_link'} > Kirish</NavLink>
+          </div>
       <div className="hamburger">
           <button type={'button'} className={'btnx btn-yellow btn_hamburger'}
-           onClick={()=>setHamburger(prev=>!prev)}>
+           onClick={isopen}>
             <GiHamburgerMenu/></button>
-         <div className={hamburger ? 'navbar_bar':'no_hamburger'}>
-            <NavLink to={'/kurslarimiz'} className={'nav_link'}> Kurslarimiz</NavLink>
-            <NavLink to={'/haqimizda'} className={'nav_link'}> Biz haqimizda</NavLink>
-            <NavLink to={'kirish'} className={'nav_link btnx btn_nav_link'} > Kirish</NavLink>
-         </div>
+            <Modal isOpen={hamburger} toggle={isopen}>
+        <ModalHeader toggle={isopen}>Menu</ModalHeader>
+        <ModalBody className='navbar_bar'>  
+              <NavLink to={'/kurslarimiz'} className={'nav_link'}> Kurslarimiz</NavLink>
+               <NavLink to={'/haqimizda'} className={'nav_link'}> Biz haqimizda</NavLink>
+               <NavLink to={'kirish'} className={'nav_link btnx btn_nav_link'} > Kirish</NavLink>
+        </ModalBody>
+      </Modal>
       </div>
       
       </div>
